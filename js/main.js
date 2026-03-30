e4b83e95dc9decfe1129024a0cfce3c8a92b4aed
Salah-mobile <tabitsalah12@gmail.com>
2026-03-30 15:22:13 +0100

    create the style of the bare search , buttons ans imges

--- js/main.js
+++ js/main.js
@@ -4,10 +4,9 @@ let categories=[... new Set(games.map(it=>(it.category)))]
 categories.unshift("All")
 let btnsS=document.getElementById('btns')
 for (let i = 0; i < categories.length; i++) {
-    console.log(categories[i]);
-    
     let btn=document.createElement("button")
     btn.innerText=categories[i]
+    btn.className="btnC"
     btnsS.appendChild(btn)
 }
 let imgs=games.map(it=>(it.image))
